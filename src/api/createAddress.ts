457d92d4b8364a252562970915dbcd3c4d9d9d3c
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-06 15:54:52 -0300

    feat(create address) route create

---
+++ src/api/createAddress.ts
@@ -0,0 +1,26 @@
+import * as bitcoin from 'bitcoinjs-lib'
+import * as ecc from 'tiny-secp256k1'
+import ECPairFactory from 'ecpair'
+import axios from 'axios'
+import { ParameterizedContext } from 'koa'
+
+export const createAddress = async (ctx: ParameterizedContext) => {
+  const ECPair = ECPairFactory(ecc)
+
+  const key = ECPair.makeRandom()
+
+  const { address } = bitcoin.payments.p2pkh({ pubkey: key.publicKey })
+  console.log('kekers')
+  const result = await axios({
+    url: `https://blockchain.info/rawaddr/${address}`,
+    method: 'GET',
+  })
+   
+  ctx.status = 200
+  ctx.body = {
+    result: result.data
+  }
+  
+  return ;
+  
+}
