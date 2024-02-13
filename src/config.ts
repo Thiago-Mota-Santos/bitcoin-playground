4c31aa7184dbc7f48271f17ee99ed07ca4204a90
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-13 17:47:55 -0300

    feat(env) add new field to config

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,20 @@
-import dotenvSafe from "dotenv-safe";
-import path from "path";
+import dotenvSafe from 'dotenv-safe'
+import path from 'path'
 
-const cwd = process.cwd();
+const cwd = process.cwd()
 
-const root = path.join.bind(cwd);
+const root = path.join.bind(cwd)
 
 dotenvSafe.config({
-  path: root(".env"),
-  sample: root(".env.example"),
-});
+  path: root('.env'),
+  sample: root('.env.example')
+})
 
 export const config = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-};
+  BASE_URL: process.env.BASE_URL
+}
+
