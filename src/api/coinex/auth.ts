989cefafb06f901ae7311ff29584f6b997d9e81d
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-13 18:28:10 -0300

    fix(ctx) fix params

--- src/api/coinex/auth.ts
+++ src/api/coinex/auth.ts
@@ -2,8 +2,13 @@ import crypto from 'crypto'
 import { config } from '../../config';
 import { ParameterizedContext } from 'koa';
 
-const createDictText = (ctx: ParameterizedContext) =>  {
-    const params = ctx.query
+type Auth = {
+    access_id: string
+    tonce: number
+    [key: string]: number | string; 
+} 
+
+const createDictText = (params: Auth) =>  {
     const keys = Object.keys(params).sort();
     let qs = keys[0] + "=" + params[keys[0]];
     for (var i = 1; i < keys.length; i++) {
@@ -13,8 +18,8 @@ const createDictText = (ctx: ParameterizedContext) =>  {
 }
 
 
-export const auth = async (ctx: ParameterizedContext) => {
-    const text = createDictText(ctx) + "&secret_key=" + config
+export const auth = (params: Auth) => {
+    const text = createDictText(params) + "&secret_key=" + config
 
     return crypto
     .createHash("md5")
