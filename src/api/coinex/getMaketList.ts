5a8debfe2313808b1bc7e870c76cc39fd59a0eed
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-13 18:18:20 -0300

    chore(url) add export to const

--- src/api/coinex/getMaketList.ts
+++ src/api/coinex/getMaketList.ts
@@ -2,16 +2,16 @@ import axios from "axios";
 import { ParameterizedContext } from "koa"
 import { config } from "../../config";
 
-const url = `${config.BASE_URL}/market/list`
+export const URL = config.BASE_URL
 
 export const getMarketList = async (ctx: ParameterizedContext) => {
     try {
-        const res = await axios.get(url);
+        const res = await axios.get(`${URL}/market/list`);
         ctx.body = {
             res: res.data
         }
     } catch (error) {
-        
+
         ctx.body = {
             error: 'An error occurred',
             status: 500
