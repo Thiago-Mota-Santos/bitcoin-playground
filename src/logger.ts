66ebd773a34cd6482f6a0d14695d0969efe9a341
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-23 15:35:03 -0300

    feat(logger) add

---
+++ src/logger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+import { config } from './src/config';
+
+const level = config.LOG_LEVEL || 'notice';
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DDTHH:mm:ss.SSSZ',
+    }),
+    winston.format.colorize(),
+    winston.format.printf(info => {
+      return `[${info.timestamp}] ${info.level}: ${info.message} ${info.stack ? info.stack : ''
+        }`;
+    })
+  ),
+  levels: winston.config.syslog.levels,
+  level,
+  transports: [
+    new winston.transports.Console({
+      handleExceptions: true,
+    }),
+  ],
+  exitOnError: false,
+});
+
+
+export { logger };
